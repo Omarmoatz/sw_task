b8179ca87bf5241d8ac0496a18c47d7b2ae6d0ef
Omarmoatz <omar.raouf.002@gmail.com>
2025-03-26 15:34:04 +0200

    design the category model

--- sw_task/categories/admin.py
+++ sw_task/categories/admin.py
@@ -1,1 +1,10 @@
+from sw_task.categories.models import Category
 from django.contrib import admin
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent"]
+    search_fields = ["name"]
+    raw_id_fields = ["parent"]
+    
--- sw_task/categories/models.py
+++ sw_task/categories/models.py
@@ -1,2 +1,29 @@
 from django.db import models
+from mptt.models import MPTTModel
+from mptt.models import TreeForeignKey
 
+
+class Category(MPTTModel):
+    """
+    Category Model using MPTT for hierarchical data
+    This model allows categories
+    to have parent-child relationships with unlimited depth.
+    It is useful for representing nested categories in an efficient way.
+    """
+
+    name = models.CharField(max_length=255)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="children",
+    )  # Self-referencing foreign key to define parent-child hierarchy
+
+    class MPTTMeta:
+        order_insertion_by = [
+            "name",
+        ]  # Ensures categories are ordered alphabetically by name
+
+    def __str__(self):
+        return self.name
